ef5db217da82314ca00920d0aa854909c676522b
Nikola Sibalic <nikolasibalic@physics.org>
2024-05-18 00:50:31 +0200

    improve visibility of code; remove outputs;

--- _static/thebew_interactive_code.js
+++ _static/thebew_interactive_code.js
@@ -3,13 +3,24 @@ function initInteractiveCode(){
     for (var i=0; i<allCode.length; i+= 1){
         if ((window.getComputedStyle(allCode[i]).getPropertyValue("display")=="block" ||
             window.getComputedStyle(allCode[i]).getPropertyValue("display") == "flow-root")
-            && allCode[i].parentElement.parentElement.parentElement.classList.contains("cell_input")){
-            console.log(allCode[i]);
-            console.log(allCode[i].parentElement.parentElement.parentElement.classList.toString());
-            allCode[i].dataset.executable = "true";
-            allCode[i].dataset.language = "python"
-        }
+            ){
+            if (allCode[i].parentElement.parentElement.parentElement.classList.contains("cell_input")){
+                allCode[i].dataset.executable = "true";
+                allCode[i].dataset.language = "python"
+            }
+            else if (allCode[i].parentElement.parentElement.parentElement.classList.contains("cell_output")){
+                allCode[i].style.display="None"; 
+            }
+        }  
+
     }
+
+    allCode = document.getElementsByClassName("cell_output");
+    for (var i=0; i<allCode.length; i+= 1){
+        allCode[i].style.display="None"; 
+    }
+
+
 
     thebelab.bootstrap(
         {
@@ -18,8 +29,45 @@ function initInteractiveCode(){
             mountStatusWidget: true,
             useJupyterLite: true,
             useBinder: false,
-            add_interrupt_button: false
+            add_interrupt_button: true,
+            codeMirrorConfig: {
+                theme: "one-dark",
+                viewportMargin: 150,
+                lineWrapping: true
+            },
           }
     )
-
+    adjustCSSRules(".CodeMirror", "height: auto");
+    adjustCSSRules(".CodeMirror", "background: #282c34");
+    adjustCSSRules(".CodeMirror", "font-family: 'Jet Brains Mono Nerd', Arial;");
 }
+
+function adjustCSSRules(selector, props, sheets){
+
+    // get stylesheet(s)
+    if (!sheets) sheets = [...document.styleSheets];
+    else if (sheets.sup){    // sheets is a string
+        let absoluteURL = new URL(sheets, document.baseURI).href;
+        sheets = [...document.styleSheets].filter(i => i.href == absoluteURL);
+        }
+    else sheets = [sheets];  // sheets is a stylesheet
+
+    // CSS (& HTML) reduce spaces in selector to one.
+    selector = selector.replace(/\s+/g, ' ');
+    const findRule = s => [...s.cssRules].reverse().find(i => i.selectorText == selector)
+    let rule = sheets.map(findRule).filter(i=>i).pop()
+
+    const propsArr = props.sup
+        ? props.split(/\s*;\s*/).map(i => i.split(/\s*:\s*/)) // from string
+        : Object.entries(props);                              // from Object
+
+    if (rule) for (let [prop, val] of propsArr){
+        // rule.style[prop] = val; is against the spec, and does not support !important.
+        rule.style.setProperty(prop, ...val.split(/ *!(?=important)/));
+        }
+    else {
+        sheet = sheets.pop();
+        if (!props.sup) props = propsArr.reduce((str, [k, v]) => `${str}; ${k}: ${v}`, '');
+        sheet.insertRule(`${selector} { ${props} }`, sheet.cssRules.length);
+        }
+    }
